46e2c08f615ba9d0291629cdbce9d3f338345a6c
Daniel Sieradski <daniel@self.agency>
2020-04-28 13:00:51 -0400

    🙄

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import mappings from './mappings'
 
-const utfu = {
-  hex(str) {
-    if (typeof str !== 'string') throw new Error('utfu requires a string to process')
-    mappings.forEach(mapping => {
-      const hex = new RegExp(`${mapping.utf8}`, 'gu')
-      str = str.replace(hex, mapping.char)
-    })
-    return str
-  },
-  txt(str) {
-    if (typeof str !== 'string') throw new Error('utfu requires a string to process')
-    mappings.forEach(mapping => {
-      const text = new RegExp(`${mapping.misrender}`, 'gu')
-      str = str.replace(text, mapping.char)
-    })
-    return str
-  }
+const hex = (str) => {
+  if (typeof str !== 'string') throw new Error('utfu requires a string to process')
+  mappings.forEach(mapping => {
+    const hex = new RegExp(`${mapping.utf8}`, 'gu')
+    str = str.replace(hex, mapping.char)
+  })
+  return str
 }
 
+const txt = (str) => {
+  if (typeof str !== 'string') throw new Error('utfu requires a string to process')
+  mappings.forEach(mapping => {
+    const text = new RegExp(`${mapping.misrender}`, 'gu')
+    str = str.replace(text, mapping.char)
+  })
+  return str
+}
 
-export default utfu
+export default { hex, txt }
